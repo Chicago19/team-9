6411ab1bb7f6f3aa935b2fc4efc872cec5438b38
Olivia Phillips <olphil99@gmail.com>
2019-09-21 00:09:00 -0500

    home done

--- cca-app/src/components/home.js
+++ cca-app/src/components/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import ButtonBox from './buttonbox.js';
+import '../css/home.css';
 
 class Home extends Component {
 
