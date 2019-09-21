c2da470e1f3628f63850aca3e78fa7f8ebc549a1
yspollen <ericspollen@gmail.com>
2019-09-21 06:47:08 -0500

    forms

--- cca-app/src/components/parents/forms.js
+++ cca-app/src/components/parents/forms.js
@@ -17,76 +17,7 @@ class Forms extends Component {
 
         <Row sm={{ size: 8, offset: 2 }}>
           <Col className='h2-col'>
-            <h2>Child Traumatic Stress</h2>
-            <h3>
-              Child traumatic stress is when children and adolescents are exposed to traumatic events or traumatic situations, and when this exposure overwhelms their ability to cope.
-            </h3>
-            <h3>
-              When children are exposed to situations where they feared for their lives, believed they could have been injured, witnessed violence, or tragically lost a loved one, they may show signs of traumatic stress.
-            </h3>
-            <br></br>
-          </Col>
-        </Row>
-
-        <Row sm={{ size: 8, offset: 2 }}>
-          <Col className='h2-col'>
-            <h2>Signs of Traumatic Stress</h2>
-            <h3>
-              Difficulty sleeping and nightmares
-            </h3>
-            <h3>
-              Refusing to go to school
-            </h3>
-            <h3>
-              Lack of appetite
-            </h3>
-            <h3>
-              Anger
-            </h3>
-            <h3>
-              Getting into fights at school or fighting with siblings
-            </h3>
-            <h3>
-              Nervousness
-            </h3>
-            <h3>
-              Play that includes recreating the event
-            </h3>
-            <br></br>
-          </Col>
-        </Row>
-
-        <Row sm={{ size: 8, offset: 2 }}>
-          <Col className='h2-col'>
-            <h2>Child Traumatic Stress</h2>
-            <h3>
-              Child traumatic stress is when children and adolescents are exposed to traumatic events or traumatic situations, and when this exposure overwhelms their ability to cope.
-            </h3>
-            <h3>
-              When children are exposed to situations where they feared for their lives, believed they could have been injured, witnessed violence, or tragically lost a loved one, they may show signs of traumatic stress.
-            </h3>
-            <br></br>
-          </Col>
-        </Row>
-
-        <Row sm={{ size: 8, offset: 2 }}>
-          <Col className='h2-col'>
-            <h2>Treating</h2>
-            <h3>
-              Education about the impact of trauma
-            </h3>
-            <h3>
-              Helping children and their parents establish or reestablish a sense of safety
-            </h3>            
-            <h3>
-              Techniques of dealing with overwhelming emotional reactions            
-            </h3>              
-            <h3>
-              An opportunity to talk about the traumatic experience in a safe, accepting environment
-            </h3>              
-            <h3>
-              Involvement, when possible, of primary caregivers in the healing process
-            </h3>  
+            <h2>[we will upload this] Crime Victim Compensation Application</h2>
             <br></br>
           </Col>
         </Row>
