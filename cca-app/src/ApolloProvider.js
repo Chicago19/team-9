22504c920fb02e239462d43e5f7b9d2a853aaa0f
gulnazsayed <gulnazsayed@Gulnazs-MacBook-Pro.local>
2019-09-21 06:57:00 -0500

    added table events

--- cca-app/src/ApolloProvider.js
+++ cca-app/src/ApolloProvider.js
@@ -6,16 +6,16 @@ import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/react-hooks'
 
 const httpLink = createHttpLink({
-    url: 'http://localost:5000'
-})
+    uri: 'http://localhost:5000'
+});
 
 const client = new ApolloClient({
     link: httpLink,
     cache: new InMemoryCache()
-})
+});
 
 export default (
     <ApolloProvider client={client}>
         <App/>
     </ApolloProvider>
-)
+);
